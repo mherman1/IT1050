7d479a604a91a241cdf7c16f85e98879d62792b3
mherman1 <misko1099@hotmail.com>
2017-01-29 20:58:12 -0500

    Lab1
    Final changes.

--- LabSolutions/Lab1/Program.cs
+++ LabSolutions/Lab1/Program.cs
@@ -29,7 +29,7 @@
             System.Console.Write("What is your base height in feet? Choose 4, 5, or 6. ");
             heightFeet = int.Parse(System.Console.ReadLine());
 
-            System.Console.Write("How many inches beyond your base height in feet are you? ");
+            System.Console.Write("How many inches beyond your base height in feet are you? Convert fractions to a decimal. ");
             heightInches = double.Parse(System.Console.ReadLine());
 
             totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
@@ -42,9 +42,18 @@
             
             canVote = age >= 18 && isCitizen;
             
-            System.Console.WriteLine("Your name: " + fullName + ".");
-            System.Console.WriteLine("Your height: " + totalHeightCM + " " + "cm.");
-            System.Console.WriteLine("You can vote: " + canVote + ".");
+            System.Console.WriteLine("Your name is " + fullName + ".");
+            System.Console.WriteLine("Your are " + totalHeightCM + " " + "cm tall.");
+
+            if(canVote == true)
+            {
+                System.Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                System.Console.WriteLine("You are not eligible to vote.");
+            }
+
             System.Console.WriteLine("Press any key to continue...");
             System.Console.ReadKey();
         }
